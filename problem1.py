a209212404b601dfb2132c561849d82c0f7f96d5
Delta Salim <salimouedz@gmail.com>
2025-11-21 22:42:41 +0000

    Problem 1 optimisée : boucle for avec calcul du nombre exact d'itération

--- problem1.py
+++ problem1.py
@@ -1,18 +1,18 @@
 """
 Problème 1 : Somme de tous les multiples de 5 ou de 7 inférieurs à 2013
-Idée : A l'aide de boucle faire la somme des multiples de 5 puis de 7 inférieurs à 2013 puis enlever les doublons
+Idée : A l'aide d'une boucle for faire la somme des multiples de 5 puis de 7 inférieurs à 2013 puis enlever les doublons
 Complexité : O(n) avec n = limit
-Durée environ : 0.000124 s
+Durée environ : 0.000033 s
 """
 
 from modules.timer import timer
 
 def sum_multiples(n, limit):
+    """Calcul la somme des multiples de n inférieur à limit"""
     s = 0
-    i = 1
-    while (n * i) < limit:
+    # multiple < limit signifie multiplicateur (i) =< limit div n
+    for i in range(1, (limit // n) + 1):
         s += n * i
-        i += 1
     return s
 
 def calc_sum_multiples2(n1, n2, limit):
